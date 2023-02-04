15d2af4f2044fe1514e6717ee2136e7aadbe2e89
Daniel Skogly <daniel@klungo.no>
2023-02-04 12:50:27 +0100

    Add godot files

---
+++ Player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+@export var HOOKSHOT_SPEED = 350.0
+@export var ROTATION_VELOCITY = 10.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var shot_position = null
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		rotate(0.01 * ROTATION_VELOCITY)
+
+	if Input.is_action_just_pressed("Shoot") and not is_on_floor():
+		shoot()
+		
+	if Input.is_action_just_released("Shoot"):
+		shot_position = null
+	
+	if shot_position:
+		var posDiff : Vector2 = shot_position - global_position
+		print(posDiff)
+		velocity = posDiff.normalized() * HOOKSHOT_SPEED
+	
+	move_and_slide()
+
+
+func shoot():
+	var point = $RayCast2D.get_collider()
+	if point:
+		shot_position = $RayCast2D.get_collision_point()
+		
+	
