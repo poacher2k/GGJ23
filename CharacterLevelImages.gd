eb186e1582a46eb794b3cdc24d55b1214f31ef87
Daniel Skogly <daniel@klungo.no>
2023-02-04 22:40:40 +0100

    Begin character images

---
+++ CharacterLevelImages.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@export var level : int
+@export var passive : ImageTexture
+@export var passive_n : ImageTexture
+@export var active : ImageTexture
+@export var active_n : ImageTexture
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
